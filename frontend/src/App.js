c843027301697516d93acbd9d57bd4885c3ff0b9
Akib-1243 <makib2395@gmail.com>
2026-03-25 11:41:00 +0600

    fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import AidRequest from './pages/AidRequest';
 import VolunteerDashboard from './pages/VolunteerDashboard';
 
@@ -7,6 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/aid-request" replace />} />
         <Route path="/aid-request" element={<AidRequest />} />
         <Route path="/volunteer" element={<VolunteerDashboard />} />
       </Routes>
